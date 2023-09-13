80ea1d5c066e29a1ff16e4ee81069196c72aca9c
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-09-13 17:54:17 +0200

    I make the API call from the Vue project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <script>
+import axios from 'axios';
+
+const endpoint = 'http://127.0.0.1:8000/api/projects/';
+
+import AppHeader from './components/AppHeader.vue';
+export default {
+  components: { AppHeader },
+  data: () => ({ projects: [] }),
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => { console.log(res.data) })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
+};
 </script>
 
 
 <template>
-  <h1>Hello World Rich</h1>
+  <AppHeader />
+  <main class="container my-4">
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.title }}</li>
+    </ul>
+  </main>
 </template>
 
 
